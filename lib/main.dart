569858919de4ea5ee39df279f8447d990eee700c
mariamsanad <mjeahs1234@gmail.com>
2021-05-20 00:14:11 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -20,12 +20,12 @@ import 'package:firebase_core/firebase_core.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
- await Firebase.initializeApp(name: "SecondaryApp",options: const FirebaseOptions(
+/* await Firebase.initializeApp(name: "SecondaryApp",options: const FirebaseOptions(
       appId: '1:138993678832:android:dac004f23f9fc1b63e0535',
       apiKey: 'AIzaSyCHgubmnroKE7goN9-TTnavGtvn2xyXh2M',
       messagingSenderId: '138993678832',
       projectId: 'covid-19senior'
-  ) );
+  ) );*/
   return runApp(MyApp());
 }
 
@@ -44,8 +44,11 @@ class MyApp extends StatelessWidget {
                 'news': (context) {
                   return News();
                 },
+                'situation': (context) {
+                  return RecordCovid();
+                },
                 'reportforuser': (context) {
-                  return CovidReportUser();
+                  return CovidReportUser(FirebaseAuth.instance.currentUser.uid.toString());
                 },
                 'companies': (context) {
                   return Companies();
