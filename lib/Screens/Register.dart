4b91c277ac1904441f59c3823ea9fbd026688720
mariamsanad <mjeahs1234@gmail.com>
2021-05-23 19:58:51 +0300

    Update Register.dart

--- lib/Screens/Register.dart
+++ lib/Screens/Register.dart
@@ -29,150 +29,152 @@ class _RegisterState extends State<Register> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: isLoading?Loading():Form(
-          key: _formKey,
-          child: Card(
-            child: Padding(
-              padding: const EdgeInsets.all(16),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: TextFormField(
-                      controller: _username,
-                      decoration: const InputDecoration(labelText: 'Username',border: OutlineInputBorder(
-                        borderRadius: const BorderRadius.all(
-                          const Radius.circular(25.0),
-                        ),
-                      ),),
-                      validator: (String? value) {
-                        if (value!.isEmpty) {
-                          return 'Please enter your username';
-                        }
-                        return null;
-                      },
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: TextFormField(
-                      controller: _emailController,
-                      decoration: const InputDecoration(labelText: 'Email',border: OutlineInputBorder(
-                        borderRadius: const BorderRadius.all(
-                          const Radius.circular(25.0),
-                        ),
-                      ),),
-                      validator: (String? value) {
-                        if (value!.isEmpty) {
-                          return 'Please enter some text';
-                        }
-                        return null;
-                      },
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: TextFormField(
-                      controller: _phone,
-                      decoration: const InputDecoration(labelText: 'Phone',border: OutlineInputBorder(
-                        borderRadius: const BorderRadius.all(
-                          const Radius.circular(25.0),
-                        ),
-                      ),),
-                      validator: (String? value) {
-                        return RegExp(r'^[0-9]{8}$').hasMatch(value!)?null:"enter a valid phone number";
+      body: isLoading?Loading():SingleChildScrollView(
+        child: Form(
+            key: _formKey,
+            child: Card(
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: TextFormField(
+                        controller: _username,
+                        decoration: const InputDecoration(labelText: 'Username',border: OutlineInputBorder(
+                          borderRadius: const BorderRadius.all(
+                            const Radius.circular(25.0),
+                          ),
+                        ),),
+                        validator: (String? value) {
+                          if (value!.isEmpty) {
+                            return 'Please enter your username';
+                          }
+                          return null;
                         },
-                      obscureText: true,
+                      ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: TextFormField(
-                      controller: _passwordController,
-                      decoration: const InputDecoration(labelText: 'Password',border: OutlineInputBorder(
-                        borderRadius: const BorderRadius.all(
-                          const Radius.circular(25.0),
-                        ),
-                      ),),
-                      validator: (String? value) {
-                        return RegExp(r'^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$').hasMatch(value!)?null:"make sure to have a minimum of 6 charachters(at least one number and one letter)";
-                      },
-                      obscureText: true,
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: TextFormField(
+                        controller: _emailController,
+                        decoration: const InputDecoration(labelText: 'Email',border: OutlineInputBorder(
+                          borderRadius: const BorderRadius.all(
+                            const Radius.circular(25.0),
+                          ),
+                        ),),
+                        validator: (String? value) {
+                          if (value!.isEmpty) {
+                            return 'Please enter some text';
+                          }
+                          return null;
+                        },
+                      ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: TextFormField(
-                      decoration: const InputDecoration(labelText: 'Confirm Password',border: OutlineInputBorder(
-                        borderRadius: const BorderRadius.all(
-                          const Radius.circular(25.0),
-                        ),
-                      ),),
-                      validator: (String? value) {
-                        if(value!=_passwordController.text){
-                          return 'passwords are not the same';
-                        }
-                      },
-                      obscureText: true,
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: TextFormField(
+                        controller: _phone,
+                        decoration: const InputDecoration(labelText: 'Phone',border: OutlineInputBorder(
+                          borderRadius: const BorderRadius.all(
+                            const Radius.circular(25.0),
+                          ),
+                        ),),
+                        validator: (String? value) {
+                          return RegExp(r'^[0-9]{8}$').hasMatch(value!)?null:"enter a valid phone number";
+                          },
+                        obscureText: true,
+                      ),
                     ),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.symmetric(vertical: 16),
-                    alignment: Alignment.center,
-                    child: SignInButtonBuilder(
-                      icon: Icons.person_add,
-                      backgroundColor: Colors.blueGrey,
-                      onPressed: () async {
-                        if (_formKey.currentState!.validate()) {
-                          setState(() {
-                            isLoading = true;
-                          });
-                          try{
-                            await methods.register(_emailController.text,_passwordController.text,'normal',_username.text,_phone.text).then((value){
-                              setState(() {
-                                isLoading = false;
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: TextFormField(
+                        controller: _passwordController,
+                        decoration: const InputDecoration(labelText: 'Password',border: OutlineInputBorder(
+                          borderRadius: const BorderRadius.all(
+                            const Radius.circular(25.0),
+                          ),
+                        ),),
+                        validator: (String? value) {
+                          return RegExp(r'^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$').hasMatch(value!)?null:"make sure to have a minimum of 6 charachters(at least one number and one letter)";
+                        },
+                        obscureText: true,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: TextFormField(
+                        decoration: const InputDecoration(labelText: 'Confirm Password',border: OutlineInputBorder(
+                          borderRadius: const BorderRadius.all(
+                            const Radius.circular(25.0),
+                          ),
+                        ),),
+                        validator: (String? value) {
+                          if(value!=_passwordController.text){
+                            return 'passwords are not the same';
+                          }
+                        },
+                        obscureText: true,
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.symmetric(vertical: 16),
+                      alignment: Alignment.center,
+                      child: SignInButtonBuilder(
+                        icon: Icons.person_add,
+                        backgroundColor: Colors.blueGrey,
+                        onPressed: () async {
+                          if (_formKey.currentState!.validate()) {
+                            setState(() {
+                              isLoading = true;
+                            });
+                            try{
+                              await methods.register(_emailController.text,_passwordController.text,'normal',_username.text,_phone.text).then((value){
+                                setState(() {
+                                  isLoading = false;
+                                });
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                    duration: const Duration(seconds: 10),
+                                    content: Text("Successfully registered"),
+                                    backgroundColor: Colors.green,
+                                    behavior: SnackBarBehavior.floating,
+                                    shape: StadiumBorder(),
+                                  ),
+                                );
+                                Navigator.of(context).pushNamed("homepage");
                               });
+
+                            }on FirebaseAuthException catch(e){
+
                               ScaffoldMessenger.of(context).showSnackBar(
                                 SnackBar(
                                   duration: const Duration(seconds: 10),
-                                  content: Text("Successfully registered"),
-                                  backgroundColor: Colors.green,
+                                  content: Text(e.message),
+                                  backgroundColor: Colors.red,
                                   behavior: SnackBarBehavior.floating,
                                   shape: StadiumBorder(),
                                 ),
                               );
-                              Navigator.of(context).pushNamed("homepage");
+
+                            }
+                            setState(() {
+                              isLoading = false;
                             });
-
-                          }on FirebaseAuthException catch(e){
-
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(
-                                duration: const Duration(seconds: 10),
-                                content: Text(e.message),
-                                backgroundColor: Colors.red,
-                                behavior: SnackBarBehavior.floating,
-                                shape: StadiumBorder(),
-                              ),
-                            );
-
                           }
-                          setState(() {
-                            isLoading = false;
-                          });
-                        }
-                      },
-                      text: 'Register',
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(30.0))) ,
+                        },
+                        text: 'Register',
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(30.0))) ,
+                      ),
                     ),
-                  ),
-                  
-                ],
+                    
+                  ],
+                ),
               ),
-            ),
-          )),
+            )),
+      ),
     );
   }
 
