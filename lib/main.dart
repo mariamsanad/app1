f10bb53e776d979bd58e682f2b5da7085e28fba1
mariamsanad <mjeahs1234@gmail.com>
2021-05-24 23:06:23 +0300

    Hi

--- lib/main.dart
+++ lib/main.dart
@@ -25,12 +25,12 @@ import 'package:firebase_core/firebase_core.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
- await Firebase.initializeApp(name: "SecondaryApp",options: const FirebaseOptions(
+ /*await Firebase.initializeApp(name: "SecondaryApp",options: const FirebaseOptions(
       appId: '1:138993678832:android:dac004f23f9fc1b63e0535',
       apiKey: 'AIzaSyCHgubmnroKE7goN9-TTnavGtvn2xyXh2M',
       messagingSenderId: '138993678832',
       projectId: 'covid-19senior'
-  ) );
+  ) );*/
   return runApp(MyApp());
 }
 
