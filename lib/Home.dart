e2c8425ef48b1c92d74729702c4d93d517824bb6
mariamsanad <mjeahs1234@gmail.com>
2021-05-25 16:11:08 +0300

    Update Home.dart

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,10 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:horizontal_card_pager/card_item.dart';
 import './Components/drawer.dart';
 import 'Services/crudUser.dart';
-import 'package:horizontal_card_pager/horizontal_card_pager.dart';
 
 
 //final GlobalKey<ScaffoldState> scaffoldKey = new GlobalKey<ScaffoldState>();
@@ -15,89 +13,48 @@ class Home extends StatelessWidget {
   //final FirebaseAuth _auth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
-
-    final List<String> titles = [
-      "RED",
-      "YELLOW",
-      "CYAN",
-      "BLUE",
-      "GREY",
-    ];
-    final List<Widget> images = [
-      Container(
-        decoration: BoxDecoration(
-          color: Colors.red,
-          borderRadius: BorderRadius.all(Radius.circular(10)),
-        ),
-      ),
-      Container(
-        decoration: BoxDecoration(
-          color: Colors.yellow,
-          borderRadius: BorderRadius.all(Radius.circular(10)),
-        ),
-      ),
-      Container(
-        decoration: BoxDecoration(
-          color: Colors.cyan,
-          borderRadius: BorderRadius.all(Radius.circular(10)),
-        ),
-      ),
-      Container(
-        decoration: BoxDecoration(
-          color: Colors.blue,
-          borderRadius: BorderRadius.all(Radius.circular(10)),
-        ),
-      ),
-      Container(
-        decoration: BoxDecoration(
-          color: Colors.grey,
-          borderRadius: BorderRadius.all(Radius.circular(10)),
-        ),
-      ),
-    ];
-
-   // final active_user = Provider.of<user>(context);
+    // final active_user = Provider.of<user>(context);
     //print(active_user);
     return StreamBuilder(
-      stream: _auth.suser,
-      builder: (context, AsyncSnapshot snapshot) {
-        print(snapshot.data);
-        return Scaffold(
-          //key: scaffoldKey,
-            drawer: MyDrawer(),
-            appBar: AppBar(
-             /* backgroundColor: Colors.cyan,*/
-              title: Text("Covid-19"),
-              actions: [
-                Container(
-                  child: FirebaseAuth.instance.currentUser==null?null:FlatButton(onPressed: ()async{
-                    await _auth.signout().then((value) => ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                      backgroundColor: Colors.green,
-                      content: Text('Signed out successfully'),
-                    )));
+        stream: _auth.suser,
+        builder: (context, AsyncSnapshot snapshot) {
+          print(snapshot.data);
+          return Scaffold(
+            //key: scaffoldKey,
+              drawer: MyDrawer(),
+              appBar: AppBar(
+                /* backgroundColor: Colors.cyan,*/
+                title: Text("Covid-19"),
+                actions: [
+                  Container(
+                    child: FirebaseAuth.instance.currentUser==null?null:FlatButton(onPressed: ()async{
+                      await _auth.signout().then((value) => ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                        backgroundColor: Colors.green,
+                        content: Text('Signed out successfully'),
+                      )));
 
-                  }, child: Text("Logout")),
-                )
-              ],
-            ),
-            body: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  /*Center(
+                    }, child: Text("Logout")),
+                  )
+                ],
+              ),
+              body: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    /*Center(
                     child: Image.asset('assets/images/download.jpg'),
                   ),*/
-                  /*Container(
+                    /*Container(
                     padding: EdgeInsets.all(10),
                     child: Text("Services: ", style: TextStyle(fontSize:30 , color: Colors.cyan),),
                   ),*/
-                  Container(
-                    padding: EdgeInsets.only(top: 30),
-                    height: 150,
-                    child: ListView(
-                      scrollDirection: Axis.horizontal ,
-                      children: [
-                       /* InkWell(
+                    Container(
+                      padding: EdgeInsets.only(top: 30),
+                      height: 150,
+                      child: ListView(
+                        scrollDirection: Axis.horizontal ,
+                        children: [
+                          /* InkWell(
                           child:Container(
                             height: 150,
                             width: 150,
@@ -109,45 +66,45 @@ class Home extends StatelessWidget {
                             Navigator.of(context).pushNamed("users");
                           },
                         ),*/InkWell(
-                          child:  Container(
-                            height: 150,
-                            width: 150,
-                            child: ListTile(
-                              title: Image.asset("assets/images/graph.png",width: 80, height: 80, ),
-                              subtitle: Text("Statistics", textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.w700),),
-                            ),), onTap: (){
+                            child:  Container(
+                              height: 150,
+                              width: 150,
+                              child: ListTile(
+                                title: Image.asset("assets/images/graph.png",width: 80, height: 80, ),
+                                subtitle: Text("Statistics", textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.w700),),
+                              ),), onTap: (){
                             Navigator.of(context).pushNamed("statistics");
-                        },
-                        ), InkWell(
-                          child:  Container(
-                            height: 100,
-                            width: 150,
-                            child: ListTile(
-                              title: Image.asset("assets/images/news.png",width: 80, height: 80, ),
-                              subtitle: Text("News", textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.w700)),
-                            ),),onTap: (){
+                          },
+                          ), InkWell(
+                            child:  Container(
+                              height: 100,
+                              width: 150,
+                              child: ListTile(
+                                title: Image.asset("assets/images/news.png",width: 80, height: 80, ),
+                                subtitle: Text("News", textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.w700)),
+                              ),),onTap: (){
                             Navigator.of(context).pushNamed("news");
-                        },
-                        ), InkWell(
-                          child: Container(
-                            height: 150,
-                            width: 150,
-                            child: ListTile(
-                              title: Image.asset("assets/images/calendar.png",width: 80, height: 80, ),
-                              subtitle: Text("My Situation", textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.w700),),
-        ),),onTap: (){},
-                        ),
-                      ] ,
+                          },
+                          ), InkWell(
+                            child: Container(
+                              height: 150,
+                              width: 150,
+                              child: ListTile(
+                                title: Image.asset("assets/images/calendar.png",width: 80, height: 80, ),
+                                subtitle: Text("My Situation", textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.w700),),
+                              ),),onTap: (){},
+                          ),
+                        ] ,
+                      ),
                     ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.all(15.0),
-                    // padding: EdgeInsets.all(15),
-                    height: 350,
-                    decoration: BoxDecoration(
-                      // border: Border.all(),
-                      borderRadius: BorderRadius.all(Radius.circular(20)),
-                      /*color: Colors.white,
+                    Container(
+                      margin: const EdgeInsets.all(15.0),
+                      // padding: EdgeInsets.all(15),
+                      height: 350,
+                      decoration: BoxDecoration(
+                        // border: Border.all(),
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                        /*color: Colors.white,
                       boxShadow: [
                         BoxShadow(
                           color: Colors.grey.withOpacity(0.5),
@@ -156,75 +113,75 @@ class Home extends StatelessWidget {
                           offset: Offset(0, 3), // changes position of shadow
                         ),
                       ],*/
-                    ),
-                   /* decoration: BoxDecoration(
+                      ),
+                      /* decoration: BoxDecoration(
                         // border: Border.all(),
                         borderRadius: BorderRadius.all(Radius.circular(20))
                     ),*/
-                    // width: 150,
-                    child: PageView(
-                      children: <Widget>[
-                        Container(
-                          margin: const EdgeInsets.only(top: 15,right: 8,left: 8,bottom: 18),
-                          decoration: BoxDecoration(
-                            // border: Border.all(),
-                            borderRadius: BorderRadius.all(Radius.circular(20)),
-                            color: Colors.white,
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.grey.withOpacity(0.5),
-                                spreadRadius: 5,
-                                blurRadius: 7,
-                                offset: Offset(0, 3), // changes position of shadow
-                              ),
-                            ],
-                          ),
-
-                        ),
-                        Container(
-                           margin: const EdgeInsets.only(top: 15,right: 8,left: 8,bottom: 18),
-                          decoration: BoxDecoration(
-                        // border: Border.all(),
-                        borderRadius: BorderRadius.all(Radius.circular(20)),
-                            color: Colors.white,
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.grey.withOpacity(0.5),
-                                spreadRadius: 5,
-                                blurRadius: 7,
-                                offset: Offset(0, 3), // changes position of shadow
-                              ),
-                            ],
-                    ),
-
-                        ),
-                        Container(
-                           margin: const EdgeInsets.only(top: 15,right: 8,left: 8,bottom: 18),
-                          decoration: BoxDecoration(
-                        // border: Border.all(),
-                        borderRadius: BorderRadius.all(Radius.circular(20)),
-                            color: Colors.white,
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.grey.withOpacity(0.5),
-                                spreadRadius: 5,
-                                blurRadius: 7,
-                                offset: Offset(0, 3), // changes position of shadow
-                              ),
-                            ],
-                    ),
-                          child: Column(
+                      // width: 150,
+                      child: PageView(
+                        children: <Widget>[
+                          Container(
+                            margin: const EdgeInsets.only(top: 15,right: 8,left: 8,bottom: 18),
+                            decoration: BoxDecoration(
+                              // border: Border.all(),
+                              borderRadius: BorderRadius.all(Radius.circular(20)),
+                              color: Colors.white,
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.withOpacity(0.5),
+                                  spreadRadius: 5,
+                                  blurRadius: 7,
+                                  offset: Offset(0, 3), // changes position of shadow
+                                ),
+                              ],
+                            ),
 
                           ),
-                        ),
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            )
-        );
-      }
+                          Container(
+                            margin: const EdgeInsets.only(top: 15,right: 8,left: 8,bottom: 18),
+                            decoration: BoxDecoration(
+                              // border: Border.all(),
+                              borderRadius: BorderRadius.all(Radius.circular(20)),
+                              color: Colors.white,
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.withOpacity(0.5),
+                                  spreadRadius: 5,
+                                  blurRadius: 7,
+                                  offset: Offset(0, 3), // changes position of shadow
+                                ),
+                              ],
+                            ),
+
+                          ),
+                          Container(
+                            margin: const EdgeInsets.only(top: 15,right: 8,left: 8,bottom: 18),
+                            decoration: BoxDecoration(
+                              // border: Border.all(),
+                              borderRadius: BorderRadius.all(Radius.circular(20)),
+                              color: Colors.white,
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.withOpacity(0.5),
+                                  spreadRadius: 5,
+                                  blurRadius: 7,
+                                  offset: Offset(0, 3), // changes position of shadow
+                                ),
+                              ],
+                            ),
+                            child: Column(
+
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              )
+          );
+        }
     );
   }
 }
